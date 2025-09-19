a9f45ca5525ecc141950bef1415b3f44338390e0
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-09-19 23:47:13 +0300

    parser better error messaging

--- source/include/app/parser.hxx
+++ source/include/app/parser.hxx
@@ -2,6 +2,8 @@
 #define APP_PARSER_HXX
 
 #include <app/tokenizer.hxx>
+#include <app/utility/error.hxx>
+#include <app/utility/print.hxx>
 
 #include <stdexcept>
 #include <string>
@@ -15,30 +17,49 @@ namespace app {
             EndOfFile,
         };
         using String = std::string;
+        using ItToken = Tokens::const_iterator;
+        // data members
         String program_name_{ };
         String main_body_{ };
         bool include_iostream_{ false };
-        Tokens::const_iterator pos_;
-        Tokens::const_iterator end_;
+        ItToken pos_;
+        ItToken end_;
+        Error error_;
         Context context_{ Context::Global };
+        // utility functions
+        void add_error( String const &message ) {
+            error_.add( message + ", but got " + ( pos_ != end_ ? app::to_string( *pos_ ) : "nothing") );
+        }
+        // processing functions
         void process_global( ) {
-            if ( pos_->kind( ) != Token::Kind::IdentifierDefault ) throw std::runtime_error( "Expected program name identifier" );
+            if ( pos_->kind( ) != Token::Kind::IdentifierDefault ) {
+                add_error( "Expected program name identifier" );
+                ++pos_;
+                return;
+            }
             program_name_ = std::string{ pos_->value( ) };
-            ++pos_;
-            if ( pos_->kind( ) != Token::Kind::KeywordProgram ) throw std::runtime_error( "Expected -program keyword" );
-            ++pos_;
-            if ( pos_->kind( ) != Token::Kind::ScopeBeg ) throw std::runtime_error( "Expected ':' token" );
+            if ( ++pos_ != end_ && pos_->kind( ) != Token::Kind::KeywordProgram ) {
+                add_error( "Expected -program keyword" );
+                ++pos_;
+                return;
+            }
+            if ( ++pos_ != end_ && pos_->kind( ) != Token::Kind::ScopeBeg ) {
+                add_error( "Expected ':'" );
+                ++pos_;
+                return;
+            }
             ++pos_;
             context_ = Context::Statement;
         }
         void process_statement( ) {
-            if ( pos_->kind( ) == Token::Kind::KeywordProgram ) throw std::runtime_error( "Nested -program is not allowed" );
+            if ( pos_->kind( ) == Token::Kind::KeywordProgram ) add_error( "Nested -program is not allowed" );
             if ( pos_->kind( ) == Token::Kind::KeywordEcho ) {
                 include_iostream_ = true;
                 ++pos_;
-                if ( pos_->kind( ) != Token::Kind::LiteralString ) throw std::runtime_error( "Expected string literal after -echo" );
+                if ( pos_ != end_ && pos_->kind( ) != Token::Kind::LiteralString ) add_error( "Expected string literal after -echo" );
                 main_body_ += "    std::cout << " + std::string{ pos_->value( ) } + ";\n";
                 ++pos_;
+                return;
             }
             if ( pos_->kind( ) == Token::Kind::Comma ) {
                 ++pos_;
@@ -49,11 +70,12 @@ namespace app {
                 ++pos_;
                 return;
             }
-            throw std::runtime_error( "Unexpected token" );
+            add_error( "Unexpected" );
             ++pos_;
         }
         void process_end_of_file( ) {
-            if ( pos_ != end_ ) throw std::runtime_error( "Expected end of file" );
+            if ( pos_ != end_ ) add_error( "Expected end of file" );
+            ++pos_;
         }
     public:
         Parser( Tokens const &tokens ) : pos_( tokens.begin( ) ), end_( tokens.end( ) ) {
@@ -65,7 +87,8 @@ namespace app {
                     default: throw std::runtime_error( "Unknown parser context" );
                 }
             }
-            if ( context_ != Context::EndOfFile ) throw std::runtime_error( "Unexpected end of file" );
+            if ( context_ != Context::EndOfFile ) add_error( "Unexpected end of file" );
+            error_.check_exception( );
         }
 
         auto str( ) -> String {
