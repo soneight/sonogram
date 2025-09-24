e736815b5068690e2feb5682aac340239509066c
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-09-24 09:21:47 +0300

    migrate to new structure (#9)

--- source/main.cxx
+++ source/main.cxx
@@ -1,6 +1,8 @@
 // app headers
 #include <app.hxx>
-// fetch headers
+#include <dirty/parser.hxx>
+#include <dirty/compiler.hxx>
+// son headers
 #include <son8/main.hxx>
 // std headers
 #include <iostream>
@@ -8,6 +10,7 @@
 #include <stdexcept>
 
 void son8::main( Args const &args ) try {
+    // print first argument (program name)
     std::cout << *args.begin( ) << std::endl;
 
     if ( args.size( ) != 2 ) throw std::runtime_error( "Expected exactly one argument - source file path" );
@@ -18,11 +21,12 @@ void son8::main( Args const &args ) try {
     std::string source{ ItStreamBuf(source_file), ItStreamBuf( ) };
     source_file.close( );
 
-    app::Compiler compiler{ std::move(source) };
+    dirty::Compiler compiler{ std::move(source) };
     std::cout << "Info: generating intermediate representation..." << std::endl;
     compiler.generate( );
     std::cout << "Info: transferring to C++17..." << std::endl;
     compiler.transfer( );
+    exit( EXIT_SUCCESS );
 } catch ( std::exception const &e ) {
     std::cerr << "Error: " << e.what( ) << std::endl;
     exit( EXIT_FAILURE );
