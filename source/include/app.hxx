f756c583901bc91834d00613db10a60db43035f3
Oleg'Ease'Kharchuk <olegease@gmail.com>
2026-05-09 10:05:34 +0200

    Source 2026-05-09 (#1)
    Reviewed-on: https://codeberg.org/soneight/sonogram/pulls/1
    max line length validation and global source string access
    all includes in app header

--- source/include/app.hxx
+++ source/include/app.hxx
@@ -1,26 +1,58 @@
 #ifndef APP_HXX
 #define APP_HXX
-
+// son8
+// -- c
 #include <son8/c/base.hxx>
-
+// -- cxx
+#include <son8/cxx/file.hxx>
+#include <son8/cxx/flow.hxx>
+#include <son8/cxx/text.hxx>
+// -- mainland
+#include <son8/main.hxx>
+// macros
 #define APP_PROC void
 #define APP_DATA static constexpr auto
 
 namespace app {
+   using namespace son8;
+   template < typename Type > using Ref = Type const &;
    // type aliases
+   // -- fundamental
    using Char = unsigned char;
-   using Size = son8::c::size_t;
+   using Size = c::size_t;
+   // -- classes
+   using String = cxx::string;
    // maximum limits
    class Max final {
       APP_DATA s = 0b1ull;
    public:
-      APP_DATA File_Size = Size{ s << 20u }; // 1 MiB
+      APP_DATA File_Size = Size{ s << 20u };    // 1 MiB
+      APP_DATA Line_Length = Size{ s << 11u };  // 2 KiB
       APP_DATA print( unsigned long long i ) -> char const * {
          switch ( i ) {
             case 1'048'576u: return "1 MiB";
+            case 2'048u: return "2 KiB";
             default: return "error";
          }
       }
+   };
+
+   class Source final {
+      bool assigned_{ };
+      String data_;
+   public:
+      Source( ) = default;
+     ~Source( ) = default;
+      Source( Source &&move ) = delete;
+      Source( Ref< Source > copy ) = delete;
+      auto operator=( Ref< Source > copy ) = delete;
+      Source( String &&str ) noexcept : data_{ son8::cxx::move( str ) } { }
+      void operator=( Source &&move ) {
+         if ( assigned_ ) throw cxx::runtime_error{ "Source could be assigned only once" };
+         data_ = cxx::move( move.data_ );
+         assigned_ = true;
+      }
+      Ref< String > get( ) { return data_; }
    };
 } // namespace app
 
--- source/main.cxx
+++ source/main.cxx
@@ -1,43 +1,75 @@
 #include <app.hxx>
-// son8
-// -- cxx
-#include <son8/cxx/file.hxx>
-#include <son8/cxx/flow.hxx>
-#include <son8/cxx/text.hxx>
-// -- mainland
-#include <son8/main.hxx>
 
-
+using Str = app::String;
+static app::Source GlobalSourceWrite_;
+static app::Ref< Str > Global_Source_Read = GlobalSourceWrite_.get( );
 
 APP_PROC son8::main( Args args ) try {
    auto constexpr New_Line = '\n';
    cxx::cout << "sonogram:\n";
    cxx::cout << "-- Max File Size: " << app::Max::print( app::Max::File_Size ) << New_Line;
+   cxx::cout << "-- Max Line Length: " << app::Max::print( app::Max::Line_Length ) << New_Line;
    cxx::cout << cxx::endl;
    // validate arguments
-   if ( args.size( ) != 2 ) throw cxx::runtime_error{ "expect exactly one argument" };
+   using Err = cxx::runtime_error;
+   if ( args.size( ) != 2 ) throw Err{ "expect exactly one argument" };
    auto fileName = *( args.begin( ) + 1 );
    namespace fs = cxx::filesystem;
    app::Size fileSize = fs::file_size( fileName );
-   if ( app::Max::File_Size < fileSize ) throw cxx::runtime_error{ "source file are too big" };
+   if ( app::Max::File_Size < fileSize ) throw Err{ "source file size exceeds maximum limit" };
    // open file
    using File = cxx::ifstream;
    File sourceFile{ fileName, cxx::ios::binary };
-   if ( not sourceFile.is_open( ) ) throw cxx::runtime_error{ "source file cannot be open" };
+   if ( not sourceFile.is_open( ) ) throw Err{ "source file cannot be open" };
    // check last byte for new line character
    auto isFileEndsWithNewLine = []( File &file ) -> bool {
-      auto lastByte = file.seekg( -1, std::ios::end ).get( );
-      file.seekg( 0, std::ios::beg );
+      auto lastByte = file.seekg( -1, cxx::ios::end ).get( );
+      file.seekg( 0, cxx::ios::beg );
       return lastByte == New_Line;
    };
-   if ( not isFileEndsWithNewLine( sourceFile ) ) throw cxx::runtime_error{ "source file does not ends with new line character" };
+   if ( not isFileEndsWithNewLine( sourceFile ) ) throw Err{ "source file does not ends with new line character" };
    // read whole file
    using ItStreamBuf = cxx::istreambuf_iterator< typename File::char_type >;
-   std::string source{ ItStreamBuf{ sourceFile }, ItStreamBuf{ } };
+   GlobalSourceWrite_ = Str{ ItStreamBuf{ sourceFile }, ItStreamBuf{ } };
    sourceFile.close( );
+   // validate maximum line length
+   auto isLineLengthValid = []( app::Ref< Str > str, auto max ) -> bool {
+      auto const end = str.end( );
+      auto beg = str.begin( );
+      auto it = beg;
+      // NOTE: Backtracking helper
+      // \ find the nearest newline and update `beg` iterator
+      // \ to avoid re-scanning verified region in the future
+      auto found = [&beg,&it]( auto const checkpoint ) -> bool {
+         while ( beg < it ) {
+            if ( *--it != New_Line ) continue;
+            ++it; // it was new line so skip it
+            beg = checkpoint + 1;
+            return true;
+         }
+         return false;
+      };
+      // NOTE: Jump-skipping loop
+      // \ short-circuit if jumps larger than buffer
+      // \ fast-paths if landing exactly on new line
+      // \ falls back to `found` backtracking helper
+      while ( it < end ) {
+         if ( end - it <= max ) return true;
+         it += max;
+         if ( *it == New_Line ) {
+            beg = ++it;
+            continue;
+         }
+         // `it` used here as local var replacement
+         if ( not found( it ) ) return false;
+      }
+      return true;
+   };
+   app::Ref< Str > source = Global_Source_Read;
+   if ( not isLineLengthValid( source, app::Max::Line_Length ) ) throw Err{ "source file contains lines with length exceeding maximum limit" };
    // TODO process file
-   std::cout << source << std::endl;
-} catch ( cxx::exception const &e ) {
+   cxx::cout << source << cxx::endl;
+} catch ( app::Ref< cxx::exception > e ) {
    cxx::cerr << "son8::main: cxx::exception: " << e.what( ) << cxx::endl;
 } catch ( ... ) {
    cxx::cerr << "... uncaught exception" << cxx::endl;
